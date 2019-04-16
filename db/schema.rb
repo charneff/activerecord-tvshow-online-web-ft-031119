ef54bb58f763ea3a40c8de6d49ecf951cf3b8e59
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-16 19:09:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 1) do
-<<<<<<< HEAD
-=======
   
   create_table "shows", force: :cascade do |t|
     t.string "name"
@@ -21,6 +19,5 @@ ActiveRecord::Schema.define(version: 1) do
     t.integer "rating"
     t.string "season"
   end
->>>>>>> ee5156a653a340bad8593742754fe7bb0be3b370
 
 end
