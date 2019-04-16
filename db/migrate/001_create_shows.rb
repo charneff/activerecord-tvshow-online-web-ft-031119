d71716519e658f819ee647972ca2dfec7feb169a
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-16 19:08:51 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-class CreateShows < ActiveRecord::Migration[5.2]
-  def change
-    create table :shows do |s|
-      s.string :name 
-      s.string :network
-      s.string :day 
-      s.integer :rating 
-    end
-  end
-    
-end 
-=======
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
